59047751541fa1ecee0bcd5f62d2e8ac66c4d826
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-05-19 14:30:09 +0530

    chumma

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ import { command } from '../commandHandler.js'
 
 export const ping = (client) => {
     command( client, ["ping"], ( message ) => {
-        message.channel.send("Pong !")
+        message.channel.send("Pong !");
     }) 
-} 
+}
